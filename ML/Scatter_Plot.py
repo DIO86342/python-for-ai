824244bdb1fdc763fe107163ae78e823dd4ff307
DIO86342 <xaajimoha9@gmail.com>
2026-09-02 14:02:32 +0300

    Add basic machine learning practice

---
+++ ML/Scatter_Plot.py
@@ -0,0 +1,15 @@
+import matplotlib
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+from scipy import stats
+
+
+# print(scipy.__version__)
+# x = np.random.normal(1,10,100)
+# y = np.random.normal(1,20,80)
+
+# plt.scatter(x,y)
+# plt.show()
+
+
