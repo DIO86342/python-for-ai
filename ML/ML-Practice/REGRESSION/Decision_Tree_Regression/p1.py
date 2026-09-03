77c120c070d3f88234c917b4e8a262883307711e
DIO86342 <xaajimoha9@gmail.com>
2026-09-03 13:42:21 +0300

    Decision Tree

---
+++ ML/ML-Practice/REGRESSION/Decision_Tree_Regression/p1.py
@@ -0,0 +1,6 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import r2_score, mean_absolute_error, mean_squared_error
+from sklearn.tree import DecisionTreeRegressor
+
+
